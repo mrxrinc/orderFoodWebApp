9c51136d9a0a4771d9089b6e19e96f9e0625c310
mohammad <mrxmirzaei@gmail.com>
2019-03-17 10:57:29 +0330

    some fixes on rinc

--- app/components/ListItem/index.js
+++ app/components/ListItem/index.js
@@ -7,8 +7,13 @@ const ListItem = () => (
       <div className="right center">
         <div className="imageBox bg" />
       </div>
-      <div className="left padd5 hCenter ">
+      <div className="left padd5 ">
         <h2 className="font bold largeText">باگت جردن</h2>
+        <div>
+          <ul className="">
+            <li className="text12 wFull reset f">ایتالیایی</li>
+          </ul>
+        </div>
       </div>
     </div>
   </div>
