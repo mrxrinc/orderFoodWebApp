2dfaea442b3c5445866d29275aad6c7edbe2d451
mohammad <mrxmirzaei@gmail.com>
2019-03-30 11:33:46 +0430

    after

--- app/components/ListItem/index.js
+++ app/components/ListItem/index.js
@@ -7,12 +7,15 @@ const ListItem = () => (
       <div className="right center">
         <div className="imageBox bg" />
       </div>
-      <div className="left padd5 ">
-        <h2 className="font bold largeText">باگت جردن</h2>
-        <div>
-          <ul className="">
-            <li className="text12 wFull reset f">ایتالیایی</li>
-          </ul>
+      <div className="left padd5 d">
+        <h2 className="font bold largeText primary">باگت جردن</h2>
+        <ul className="flex debug">
+          <li className="text12">ایتالیایی</li>
+          <li className="text12">ایتالیایی</li>
+          <li className="text12">ایتالیایی</li>
+        </ul>
+        <div className="flex economy">
+          <span className="chilivery-economic_level3 black f"></span>
         </div>
       </div>
     </div>
