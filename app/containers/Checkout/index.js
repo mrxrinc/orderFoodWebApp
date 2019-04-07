d98d3df9204db33e7c5d99b133cc73b4068f8738
mohammad <mrxmirzaei@gmail.com>
2019-04-07 16:22:39 +0430

    user cash balance

--- app/containers/Checkout/index.js
+++ app/containers/Checkout/index.js
@@ -1,18 +1,10 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
-import { compose } from 'redux';
-
-import injectSaga from 'utils/injectSaga';
-import injectReducer from 'utils/injectReducer';
-import makeSelectCheckout from './selectors';
-import reducer from './reducer';
-import saga from './saga';
 import './style.scss';
 
 import CheckoutCardItem from '../../components/CheckoutCardItem';
 import { AnimateField } from '../../components/ChiliForm';
 import GiftCode from '../../components/GiftCode';
+import UserCacheBalance from '../../components/UserCacheBalance';
 
 /* eslint-disable react/prefer-stateless-function */
 export class Checkout extends React.PureComponent {
@@ -68,31 +60,10 @@ export class Checkout extends React.PureComponent {
           />
         </div>
         <GiftCode />
+        <UserCacheBalance />
       </div>
     );
   }
 }
 
-const mapStateToProps = createStructuredSelector({
-  checkout: makeSelectCheckout(),
-});
-
-function mapDispatchToProps(dispatch) {
-  return {
-    dispatch,
-  };
-}
-
-const withConnect = connect(
-  mapStateToProps,
-  mapDispatchToProps,
-);
-
-const withReducer = injectReducer({ key: 'checkout', reducer });
-const withSaga = injectSaga({ key: 'checkout', saga });
-
-export default compose(
-  withReducer,
-  withSaga,
-  withConnect,
-)(Checkout);
+export default Checkout;
