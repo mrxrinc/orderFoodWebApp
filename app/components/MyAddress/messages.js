95de2d114dc7e95531df2d05f5e8b8af91acf5f8
mohammad <mrxmirzaei@gmail.com>
2019-04-09 16:09:44 +0430

    design my address

---
+++ app/components/MyAddress/messages.js
@@ -0,0 +1,16 @@
+/*
+ * MyAddress Messages
+ *
+ * This contains all the text for the MyAddress component.
+ */
+
+import { defineMessages } from 'react-intl';
+
+export const scope = 'app.components.MyAddress';
+
+export default defineMessages({
+  header: {
+    id: `${scope}.header`,
+    defaultMessage: 'This is the MyAddress component!',
+  },
+});
