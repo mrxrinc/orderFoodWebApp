c5a15f09193d4b432c0276e8bb4f36ed3a3aeb2f
mohammad <mrxmirzaei@gmail.com>
2019-04-10 12:46:53 +0430

    Motochili false

--- app/components/ChiliFooter/index.js
+++ app/components/ChiliFooter/index.js
@@ -20,7 +20,7 @@ class ChiliFooter extends React.Component {
 
   componentDidMount() {
     this.props.showModal({
-      motochiliModal: true,
+      motochiliModal: false,
       forGotModal: false,
       resetPassModal: false,
       successChangePassModal: false,
