cb125fcf35afd9cc03657246c9488ed7e1ab52ea
mohammad <mrxmirzaei@gmail.com>
2019-04-14 17:12:44 +0430

    created redux/saga for user login

--- app/api/constants/global/index.js
+++ app/api/constants/global/index.js
@@ -2,20 +2,23 @@ export const API_URL_ROOT = process.env.APP_URL;
 const APP_BASE_CANONICAL_URL = process.env.APP_BASE_CANONICAL_URL;
 
 export const websiteUrl = (relativePath = '') => {
+  if (relativePath === '/') {
+    relativePath = '';
+  }
 
-    if (relativePath === '/') {
-        relativePath = '';
-    }
-
-    return APP_BASE_CANONICAL_URL + relativePath;
+  return APP_BASE_CANONICAL_URL + relativePath;
 };
 
 export const globalHeader = () => {
-    if (localStorage.authToken) {
-        return {            
-            'Authorization': 'Bearer ' + localStorage.getItem('authToken')
-        };
-    } else {
-        return {};
-    }
+  console.log("!!!!!",localStorage);
+  if (localStorage.authToken) {
+    return {            
+      'Authorization': `Bearer ${  localStorage.getItem('authToken')}`,
+    };
+  } 
+  return {
+    token: 'A8A8FFD0-FF3E-4A20-847C-28CA5CE8A652',
+    'Content-Type': 'application/json',
+  };
+    
 };
