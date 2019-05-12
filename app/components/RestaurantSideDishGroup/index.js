938ea72be710effd15daf1baf37633cb9bdd664c
mohammad <mrxmirzaei@gmail.com>
2019-05-12 09:31:08 +0430

    fix sepration of restaurant and modal

--- app/components/RestaurantSideDishGroup/index.js
+++ app/components/RestaurantSideDishGroup/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import './style.scss';
 
-const RestaurantSideDishGroup = props => (
-  <div className="sideDishGroup bottomM20">
-    <div className="head hCenter vP10 rightP20 gray6">
-      <span className={`text12 ${!props.isValid ? 'text-danger' : ''}`}>
-        {props.title}
-      </span>
+const RestaurantSideDishGroup = props =>{
+  console.log(props);
+  return (
+    <div className="sideDishGroup bottomM20">
+      <div className="head hCenter vP10 rightP20 gray6">
+        <span
+          className={`text12 ${props.isValid === false ? 'text-danger' : ''}`}
+        >
+          {props.title}
+        </span>
+      </div>
+      <div className="items">{props.children}</div>
     </div>
-    <div className="items">{props.children}</div>
-  </div>
-);
+  );
+} 
 
 export default RestaurantSideDishGroup;
