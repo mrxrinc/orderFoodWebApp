c8790eeb409dda913ab3a16b5580a1bd57963f2f
mohammad <mrxmirzaei@gmail.com>
2019-05-15 17:12:45 +0430

    Add Restaurant List Search

--- app/containers/Page404/index.js
+++ app/containers/Page404/index.js
@@ -6,7 +6,7 @@ const Page404 = () => (
     <div className="order">
       <div className="order-empty__icon bottomP15">
         <img className="order-empty__img" src={icon} alt=""/>
-        <span className="order-empty__title">سبد خرید شما خالی است!</span>
+        <span className="order-empty__title">این صفحه وجود ندارد</span>
       </div>
       <Link to="/" className="btn btn-danger">
         بازگشت به صفحه اصلی
--- app/containers/RestaurantsList/index.js
+++ app/containers/RestaurantsList/index.js
@@ -4,10 +4,11 @@ import RestaurantsListItem from '../../components/RestaurantsListItem/index';
 import { restaurantSearch } from '../../api/application/restaurant';
 import Loading from '../../components/ChiliLoading';
 import NavigationBar from '../../components/NavigationBar';
-import ChiliLoading from '../../components/ChiliLoading';
-import { connect } from 'react-redux';
 import { getRegionBySlug } from '../../api/application/region';
+import SearchInput, { createFilter } from 'react-search-input';
+import icon from '../../images/icons/search_no_result.png';
 
+const KEYS_TO_FILTERS = ['name'];
 
 import './style.scss';
 /* eslint-disable react/prefer-stateless-function */
@@ -19,8 +20,13 @@ export default class RestaurantsList extends React.PureComponent {
       restaurantList: [],
       citySlug: props.match.params.citySlug,
       pointSlug: props.match.params.pointSlug,
+      searchTerm: '',
       // tag: '756',
     };
+  }
+
+  searchUpdated = (term)=> {
+    this.setState({searchTerm: term})
   }
 
   componentDidMount() {
@@ -33,7 +39,6 @@ export default class RestaurantsList extends React.PureComponent {
             `${mapCenter.lat},${mapCenter.lon}`,
             this.state.tag).then(
               response => {
-              console.log('LIST ===>>> ', response.result.data);
               const restaurantList = response.result.data;
               this.setState({ restaurantList, loading: false });
             }
@@ -49,6 +54,7 @@ export default class RestaurantsList extends React.PureComponent {
 
   render() {
     const { restaurantList } = this.state;
+    const filteredRestaurant = restaurantList.filter(createFilter(this.state.searchTerm, KEYS_TO_FILTERS))
     return (
       <div className="lightBg">
         <NavigationBar 
@@ -60,12 +66,36 @@ export default class RestaurantsList extends React.PureComponent {
           // like
           background
         />
+        {!this.state.loading &&
+          <div className="restaurant-list__search-input bgWhite">
+            <SearchInput
+              className="location__user-position-search-input center"
+              onChange={this.searchUpdated}
+              placeholder="جستجو ..."
+            />
+          </div>
+        }
         <div className="padd15 rtl">
           {this.state.loading ? 
             <Loading /> :
-            restaurantList.map((item, index) => (
-              <RestaurantsListItem key={index} {...item} />
-            ))
+            <React.Fragment>
+              {filteredRestaurant.length == 0 ?
+                  <div className="order-empty center"
+                    style={{ height: 'calc(100vh - 200px)'}}
+                  >
+                    <div className="order">
+                      <div className="order-empty__icon bottomP15">
+                        <img className="order-empty__img" src={icon} alt=""/>
+                        <span className="order-empty__title">این رستوران در این مکان وجود ندارد</span>
+                      </div>
+                    </div>
+                  </div> :
+                  filteredRestaurant.map((item, index) => (
+                    <RestaurantsListItem key={index} {...item} />
+                  ))
+              }
+
+            </React.Fragment>
           }
         </div>
       </div>
