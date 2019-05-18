bc2f5c341759a38bf8502033022618a0b31b03b8
mohammad <mrxmirzaei@gmail.com>
2019-05-18 12:21:58 +0430

    revert activation code

--- app/containers/ActivationCode/index.js
+++ app/containers/ActivationCode/index.js
@@ -98,10 +98,10 @@ class ActivationCode extends React.Component {
         <form className="activationCode__form" onSubmit={this.sendSubmit}>
           <Container className="text-center">
             <Row>
-              <Col>
+              <Col xs={12}>
                 <Input
                   className="text-center activationCode__letter-spacing"
-                  maxLength="1"
+                  maxLength="4"
                   type="text"
                   pattern="\d*"
                   id="activationCode__input-fourth"
@@ -110,42 +110,6 @@ class ActivationCode extends React.Component {
                   onChange={this.update}
                 />{' '}
               </Col>
-              <Col>
-                <Input
-                  className="text-center activationCode__letter-spacing"
-                  maxLength="1"
-                  type="text"
-                  pattern="\d*"
-                  id="activationCode__input-fourth"
-                  disabled={this.state.disabled ? 'disabled' : ''}
-                  bsSize="lg"
-                  onChange={this.update}
-                />{' '}
-              </Col>
-              <Col>
-                <Input
-                  className="text-center activationCode__letter-spacing"
-                  maxLength="1"
-                  type="text"
-                  pattern="\d*"
-                  id="activationCode__input-fourth"
-                  disabled={this.state.disabled ? 'disabled' : ''}
-                  bsSize="lg"
-                  onChange={this.update}
-                />{' '}
-              </Col>
-              <Col>
-                <Input
-                  className="text-center activationCode__letter-spacing"
-                  maxLength="1"
-                  type="text"
-                  pattern="\d*"
-                  id="activationCode__input-fourth"
-                  disabled={this.state.disabled ? 'disabled' : ''}
-                  bsSize="lg"
-                  onChange={this.update}
-                />{' '}
-              </Col>                                          
             </Row>
 
             <p className="midText mt-4">
